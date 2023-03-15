8796f744da2a41db1643e37fd0f2d48e0554a935
Rohit <122895165+rplatotech@users.noreply.github.com>
2023-03-15 10:31:47 -0400

    Update test_sql_assignment-2-3-4.py

--- test_sql_assignment-2-3-4.py
+++ test_sql_assignment-2-3-4.py
@@ -3,6 +3,7 @@ import expected_lists_for_questions
 import pymysql.cursors
 import decimal
 import datetime
+from gen_results_from_excel_queries import sql_logs_reader, sql_logs_clear
 
 
 def sql_things(query):
@@ -31,7 +32,7 @@ def sql_things(query):
     # Define the database connection parameters
     host = '127.0.0.1'
     user = 'root'
-    password = 'your-password'
+    password = 'Azxsw21q!'
     database = 'sql_invoicing'
 
     # Connect to the database
@@ -76,28 +77,28 @@ def test_a2_q1():
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q1, 'list not matching'
+        assert row in expected_lists_for_questions.el_q1, 'Query error. Check manually'
 
 def test_a2_q2():
     sql_things(query=read_query_from_excel('A2'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q2, 'list not matching'
+        assert row in expected_lists_for_questions.el_q2, 'Query error. Check manually'
 
 def test_a2_q3():
     sql_things(query=read_query_from_excel('A3'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q3, 'list not matching'
+        assert row in expected_lists_for_questions.el_q3, 'Query error. Check manually'
 
 def test_a2_q4():
     sql_things(query=read_query_from_excel('A4'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q4, 'list not matching'
+        assert row in expected_lists_for_questions.el_q4, 'Query error. Check manually'
 
 
 def test_a2_q5():
@@ -105,14 +106,14 @@ def test_a2_q5():
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q5, 'list not matching'
+        assert row in expected_lists_for_questions.el_q5, 'Query error. Check manually'
 
 def test_a2_q6():
     sql_things(query=read_query_from_excel('A6'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q6, 'list not matching'
+        assert row in expected_lists_for_questions.el_q6, 'Query error. Check manually'
 
 
 def test_a2_q7():
@@ -120,56 +121,56 @@ def test_a2_q7():
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q7, 'list not matching'
+        assert row in expected_lists_for_questions.el_q7, 'Query error. Check manually'
 
 def test_a2_q8():
     sql_things(query=read_query_from_excel('A8'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q8, 'list not matching'
+        assert row in expected_lists_for_questions.el_q8, 'Query error. Check manually'
 
 def test_a2_q9():
     sql_things(query=read_query_from_excel('A9'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q9, 'list not matching'
+        assert row in expected_lists_for_questions.el_q9, 'Query error. Check manually'
 
 def test_a2_q10():
     sql_things(query=read_query_from_excel('A10'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q10, 'list not matching'
+        assert row in expected_lists_for_questions.el_q10, 'Query error. Check manually'
 
 def test_a2_q11():
     sql_things(query=read_query_from_excel('A11'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q11, 'list not matching'
+        assert row in expected_lists_for_questions.el_q11, 'Query error. Check manually'
 
 def test_a2_q12():
     sql_things(query=read_query_from_excel('A12'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q12, 'list not matching'
+        assert row in expected_lists_for_questions.el_q12, 'Query error. Check manually'
 
 def test_a2_q13():
     sql_things(query=read_query_from_excel('A13'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q13, 'list not matching'
+        assert row in expected_lists_for_questions.el_q13, 'Query error. Check manually'
 
 def test_a2_q14():
     sql_things(query=read_query_from_excel('A14'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q14, 'list not matching'
+        assert row in expected_lists_for_questions.el_q14, 'Query error. Check manually'
 
 
 def test_a2_q15():
@@ -177,7 +178,7 @@ def test_a2_q15():
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el_q15, 'list not matching'
+        assert row in expected_lists_for_questions.el_q15, 'Query error. Check manually'
 
 # ASSIGNMENT 3 ---------------------------------------------
 
@@ -186,117 +187,148 @@ def test_a3_q1():
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q1, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q1, 'Query error. Check manually'
 
 def test_a3_q2():
     sql_things(query=read_query_from_excel('A17'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q2, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q2, 'Query error. Check manually'
 
 def test_a3_q3():
     sql_things(query=read_query_from_excel('A18'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q3, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q3, 'Query error. Check manually'
 
 def test_a3_q4():
     sql_things(query=read_query_from_excel('A19'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q4, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q4, 'Query error. Check manually'
 
 def test_a3_q5():
     sql_things(query=read_query_from_excel('A20'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q5, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q5, 'Query error. Check manually'
 
 def test_a3_q6():
     sql_things(query=read_query_from_excel('A21'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q6, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q6, 'Query error. Check manually'
 
 def test_a3_q7():
     sql_things(query=read_query_from_excel('A22'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q7, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q7, 'Query error. Check manually'
 
 def test_a3_q8():
     sql_things(query=read_query_from_excel('A23'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q8, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q8, 'Query error. Check manually'
 
 def test_a3_q9():
     sql_things(query=read_query_from_excel('A24'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q9, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q9, 'Query error. Check manually'
 
 def test_a3_q10():
     sql_things(query=read_query_from_excel('A25'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el3_q10, 'list not matching'
+        assert row in expected_lists_for_questions.el3_q10, 'Query error. Check manually'
 #
-# # ASSIGNMENT 4 -------------------------------------- Q1-Q3 are just Insert, Update & Delete and no output data -----
+# # ASSIGNMENT 4 --------------------------------------
+
+def test_a4_q1():
+    sql_things(query=read_query_from_excel('A26'))
+    # print(read_query_from_excel('A26'))
+    print('\n')
+    for row in rows:
+        print(row)
+        print(sql_logs_reader())
+        assert sql_logs_reader() in expected_lists_for_questions.el4_q1
+    sql_logs_clear()
+
+def test_a4_q2():
+    sql_things(query=read_query_from_excel('A27'))
+    # print(read_query_from_excel('A26'))
+    print('\n')
+    for row in rows:
+        print(row)
+        print(sql_logs_reader())
+        assert sql_logs_reader() in expected_lists_for_questions.el4_q2
+    sql_logs_clear()
+
+def test_a4_q3():
+    sql_things(query=read_query_from_excel('A28'))
+    # print(read_query_from_excel('A26'))
+    print('\n')
+    for row in rows:
+        print(row)
+        print(sql_logs_reader())
+        assert sql_logs_reader() in expected_lists_for_questions.el4_q3
+    # sql_logs_clear()
+
 def test_a4_q4():
     sql_things(query=read_query_from_excel('A29'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el4_q4, 'list not matching'
+        assert row in expected_lists_for_questions.el4_q4, 'Query error. Check manually'
 
 def test_a4_q5():
     sql_things(query=read_query_from_excel('A30'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el4_q5, 'list not matching'
+        assert row in expected_lists_for_questions.el4_q5, 'Query error. Check manually'
 
 def test_a4_q6():
     sql_things(query=read_query_from_excel('A31'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el4_q6, 'list not matching'
+        assert row in expected_lists_for_questions.el4_q6, 'Query error. Check manually'
 
 def test_a4_q7():
     sql_things(query=read_query_from_excel('A32'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el4_q7, 'list not matching'
+        assert row in expected_lists_for_questions.el4_q7, 'Query error. Check manually'
 
 def test_a4_q8():
     sql_things(query=read_query_from_excel('A33'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el4_q8, 'list not matching'
+        assert row in expected_lists_for_questions.el4_q8, 'Query error. Check manually'
 
 def test_a4_q9():
     sql_things(query=read_query_from_excel('A34'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el4_q9, 'list not matching'
+        assert row in expected_lists_for_questions.el4_q9, 'Query error. Check manually'
 
 def test_a4_q10():
     sql_things(query=read_query_from_excel('A35'))
     print('\n')
     for row in rows:
         print(row)
-        assert row in expected_lists_for_questions.el4_q10, 'list not matching'
+        assert row in expected_lists_for_questions.el4_q10, 'Query error. Check manually'
